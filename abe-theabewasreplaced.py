16a0674b57ce2ef04fbe7d4fb06bcd0e985173c5
notadragon <berne@notadragon.com>
2026-06-21 13:10:02 -0400

    early attempts

--- abe-theabewasreplaced.py
+++ abe-theabewasreplaced.py
@@ -135,15 +135,203 @@ def opening_title(loop, args):
     loop.set_alarm_in(0.1, title_update, 1)
     loop.widget = fill
 
+# --------------------------------------------------------------------------------------------------
+class LineWalker(urwid.ListWalker):
+    """ListWalker-compatible class for lazily reading file contents."""
+
+    def __init__(self, name: str) -> None:
+        # do not overcomplicate example
+        self.file = open(name, encoding="utf-8")  # noqa: SIM115  # pylint: disable=consider-using-with
+        self.lines = []
+        self.focus = 0
+
+    def get_focus(self):
+        return self._get_at_pos(self.focus)
+
+    def set_focus(self, focus) -> None:
+        self.focus = focus
+        self._modified()
+
+    def get_next(self, position: int) -> tuple[urwid.Edit, int] | tuple[None, None]:
+        return self._get_at_pos(position + 1)
+
+    def get_prev(self, position: int) -> tuple[urwid.Edit, int] | tuple[None, None]:
+        return self._get_at_pos(position - 1)
+
+    def read_next_line(self) -> str:
+        """Read another line from the file."""
+
+        next_line = self.file.readline()
+
+        if not next_line or next_line[-1:] != "\n":
+            # no newline on last line of file
+            self.file = None
+        else:
+            # trim newline characters
+            next_line = next_line[:-1]
+
+        edit = urwid.Edit("", next_line, allow_tab=True)
+        edit.edit_pos = 0
+        edit.original_text = next_line
+        self.lines.append(edit)
+
+        return next_line
+
+    def _get_at_pos(self, pos: int) -> tuple[urwid.Edit, int] | tuple[None, None]:
+        """Return a widget for the line number passed."""
+
+        if pos < 0:
+            # line 0 is the start of the file, no more above
+            return None, None
+
+        if len(self.lines) > pos:
+            # we have that line so return it
+            return self.lines[pos], pos
+
+        if self.file is None:
+            # file is closed, so there are no more lines
+            return None, None
+
+        assert pos == len(self.lines), "out of order request?"  # noqa: S101  # "assert" is OK in examples
+
+        self.read_next_line()
+
+        return self.lines[-1], pos
+
+    def split_focus(self) -> None:
+        """Divide the focus edit widget at the cursor location."""
+
+        focus = self.lines[self.focus]
+        pos = focus.edit_pos
+        edit = urwid.Edit("", focus.edit_text[pos:], allow_tab=True)
+        edit.original_text = ""
+        focus.set_edit_text(focus.edit_text[:pos])
+        edit.edit_pos = 0
+        self.lines.insert(self.focus + 1, edit)
+
+    def combine_focus_with_prev(self) -> None:
+        """Combine the focus edit widget with the one above."""
+
+        above, _ = self.get_prev(self.focus)
+        if above is None:
+            # already at the top
+            return
+
+        focus = self.lines[self.focus]
+        above.set_edit_pos(len(above.edit_text))
+        above.set_edit_text(above.edit_text + focus.edit_text)
+        del self.lines[self.focus]
+        self.focus -= 1
+
+    def combine_focus_with_next(self) -> None:
+        """Combine the focus edit widget with the one below."""
+
+        below, _ = self.get_next(self.focus)
+        if below is None:
+            # already at bottom
+            return
+
+        focus = self.lines[self.focus]
+        focus.set_edit_text(focus.edit_text + below.edit_text)
+        del self.lines[self.focus + 1]
+
+class EditDisplay(urwid.Frame):
+
+    palette: typing.ClassVar[list[tuple[str, str, str, ...]]] = [
+        ("body", "default", "default"),
+        ("foot", "dark cyan", "dark blue", "bold"),
+        ("key", "light cyan", "dark blue", "underline"),
+    ]
+
+    footer_text = (
+        "foot",
+        [
+            "Text Editor    ",
+            ("key", "F5"),
+            " save  ",
+            ("key", "F8"),
+            " done",
+        ],
+    )
+
+    def __init__(self, loop, name: str) -> None:
+        self.loop = loop
+        self.save_name = name
+        self.walker = LineWalker(name)
+        self.listbox = urwid.ListBox(self.walker)
+        self.footer = urwid.AttrMap(urwid.Text(self.footer_text), "foot")
+        super().__init__(urwid.AttrMap(self.listbox, "body"), footer=self.footer)
+
+    def keypress(self, size, k):
+        """Last resort for keypresses."""
+
+        if k == "f5":
+            self.save_file()
+        elif k == "f8":
+            self.save_file()
+            main_menu(self.loop,None)
+        elif k == "delete":
+            # delete at end of line
+            self.walker.combine_focus_with_next()
+        elif k == "backspace":
+            # backspace at beginning of line
+            self.walker.combine_focus_with_prev()
+        elif k == "enter":
+            # start new line
+            self.walker.split_focus()
+            # move the cursor to the new line and reset pref_col
+            self.loop.process_input(["down", "home"])
+        elif k == "right":
+            w, pos = self.walker.get_focus()
+            w, pos = self.walker.get_next(pos)
+            if w:
+                self.listbox.set_focus(pos, "above")
+                self.loop.process_input(["home"])
+        elif k == "left":
+            w, pos = self.walker.get_focus()
+            w, pos = self.walker.get_prev(pos)
+            if w:
+                self.listbox.set_focus(pos, "below")
+                self.loop.process_input(["end"])
+        else:
+            return super().keypress(size, k)
+
+    def save_file(self) -> None:
+        """Write the file out to disk."""
+
+        lines = []
+        walk = self.walker
+        for edit in walk.lines:
+            # collect the text already stored in edit widgets
+            lines.append(edit.edit_text)
+
+        # then the rest
+        while walk.file is not None:
+            lines.append(walk.read_next_line())
+
+        # write back to disk
+        with open(self.save_name, "w", encoding="utf-8") as outfile:
+            prefix = ""
+            for line in lines:
+                outfile.write(prefix + line)
+                prefix = "\n"
+
+
 def edit_program(loop):
-    pass
+    editor = EditDisplay(loop, "abewasreplaced.program")
+    loop.widget = editor
+
+# --------------------------------------------------------------------------------------------------
 
 def run_program(loop):
     pass
 
+# --------------------------------------------------------------------------------------------------
+
 def buy_upgrades(loop):
     pass
 
+# --------------------------------------------------------------------------------------------------
 
 class FileListIterator(urwid.ListWalker):
     def __init__(self, text):
